32149d649a5528e6b70d9fe53955f623fdb42f39
k-san-official <konstantin.sandfort@gmail.com>
2022-04-11 13:46:31 +0200

    initial structure for project1

---
+++ project1/main.py
@@ -0,0 +1,32 @@
+import cv2
+import utils
+
+"""
+Introduction to Image and Video Processing
+------------------------------------------
+Project 1:  spatial filtering, processing
+Date:       04/2022
+Author:     Konstantin Sandfort
+------------------------------------------
+This class gets executed at the start to give results for all exercises in the project
+Algorithms for certain calculations are stored in utils.py to make the code more readable.
+"""
+
+if __name__ == "__main__":
+    # 1 Color Spaces
+    rgb_1_1 = cv2.imread("img/img_1_1.jpg", cv2.IMREAD_COLOR)
+    rgb_1_2 = cv2.imread("img/img_1_2.jpg", cv2.IMREAD_COLOR)
+
+    cv2.imshow("1 - RGB Bright Colours", rgb_1_1)
+    cv2.imshow("1 - RGB Pale Colours", rgb_1_2)
+
+    # (1) Transformation from RGB to HSV
+    hsv_1_1 = cv2.cvtColor(rgb_1_1, cv2.COLOR_RGB2HSV)
+    hsv_1_2 = cv2.cvtColor(rgb_1_2, cv2.COLOR_RGB2HSV)
+
+    cv2.imshow("1 - HSV Bright Colours", hsv_1_1)
+    cv2.imshow("1 - HSV Pale Colours", hsv_1_2)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    # (2)
---
+++ project1/utils.py
@@ -0,0 +1,8 @@
+"""
+Algorithms to solve image processing tasks are stored here.
+"""
+
+
+def rgb2hsi(img):
+    # Translates an RGB image to an HSI image - Color Spaces (2)
+    pass
