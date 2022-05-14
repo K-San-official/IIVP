c9a7e58b1c15e36b56ac54f98610abcec185a7b5
k-san-official <konstantin.sandfort@gmail.com>
2022-05-14 14:09:13 +0200

    progress for ex1

--- project2/app.py
+++ project2/app.py
@@ -7,30 +7,20 @@ import numpy as np
 Methods: In this section, all relevant methods are stored.
 """
 
+
 def save_image(name, file):
     path = "output/" + name + ".jpg"
     cv2.imwrite(path, file)
 
-def motion_blur_noise(fft):
-    new_img = fft.copy()
-    height, width, channels = fft.shape
-    print(height, width, channels)
-    #height = int(width/10)
-    #width = int(width/10)
-    a = 0.0002
-    b = 0.0002
-    for u in range(0, height):
-        print(u)
-        for v in range(0, width):
-            for c in range(0, channels):
-                new_value = (np.sinc((a*u) + (b*v)))*np.exp(-1j*np.pi*((a*u) + (b*v)))
-                new_img[u, v, c] = new_value
-                #print(new_value)
-    magnitude_spectrum1 = (20 * np.log(np.abs(new_img)))*255*255
-    cv2.imshow("MS", magnitude_spectrum1)
-    return new_img
 
-    #new_img[0:height, 0:width, 0:channels] = np.sinc()*ex
+def motion_blur_filter(h, w):
+    [u, v] = np.mgrid[-h/2:h/2, -w/2:w/2]
+    u = 2 * u/h
+    v = 2 * v/w
+    a = 0.22  # alpha
+    b = 0.22  # beta
+    h = np.sinc((a * u + b * v)) * np.exp(-1j * np.pi * (a * u + b * v))
+    return h
 
 # ----------------------------------------------------------------------------------------------------------------------
 
@@ -38,20 +28,60 @@ if __name__ == "__main__":
     # Read images for the exercises
 
     img_1_1 = cv2.imread("img/bird.jpg")
+    img_1_1 = img_1_1.astype(np.double)
     img_1_2 = cv2.imread("img/geese.jpg")
-
+    img_1_2 = img_1_2.astype(np.double)
+    cv2.imshow("Original", img_1_1/255)
 
     # --- Exercise 1 ---------------------------------------------------------------------------------------------------
 
-    fft_1_1 = np.fft.fft2(img_1_1)
-    fft_1_1_shifted = np.fft.fftshift(fft_1_1)
-    blur_filter = motion_blur_noise(fft_1_1_shifted)
-    magnitude_spectrum = (20 * np.log(np.abs(fft_1_1_shifted)))/255
-    cv2.imshow("Mag", magnitude_spectrum)
-    filter_back = np.fft.ifftshift(fft_1_1_shifted)
-    img_back = np.fft.ifft2(filter_back)
-    img_back = np.real(img_back)/255
-    cv2.imshow("Back", img_back)
+    cv2.imshow("Original", img_1_1/255)
+
+    # Convert images to frequency domain
+    f_1_1 = np.fft.fft2(img_1_1)
+
+    # Create motion blur filter
+    height1, width1, channels1 = f_1_1.shape
+    blur_filter1 = motion_blur_filter(height1, width1)
+    # Add third dimension for 3 channels of the filter
+    h1 = np.repeat(blur_filter1[:, :, np.newaxis], 3, axis=2)
+
+    # Add filter to image
+    g1 = f_1_1*h1
+
+    # Convert back to spatial domain
+    img_1_1_motion_blur = np.fft.ifft2(g1)
+    img_1_1_motion_blur = np.real(img_1_1_motion_blur)/255
+    cv2.imshow("Back", img_1_1_motion_blur)
+
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+    """
+    x = cv2.imread("img/bird.jpg")
+    x = x.astype(np.double)
+
+    n2, n1, c = x.shape
+
+    [k1, k2] = np.mgrid[0:n2, 0:n1]
+
+    [u, v] = np.mgrid[-n2 / 2:n2 / 2, -n1 / 2:n1 / 2]
+    u = 2 * u / n2
+    v = 2 * v / n1
+
+    F = np.fft.fft2(x)
+
+    a = 0.22
+    b = 0.22
+
+    # Blurring function.
+    H = np.sinc((u * a + v * b)) * np.exp(-1j * np.pi * (u * a + v * b))
+    H = np.repeat(H[:, :, np.newaxis], 3, axis=2)
+    G = F*H
+    # Motion Blurred Image.
+    g = np.fft.ifft2(G)
+    cv2.imshow("motion blur", np.abs(g)/255)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    """
 
